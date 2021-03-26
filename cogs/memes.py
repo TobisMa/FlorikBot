4ee4c0b98a0c57a1653d952c7186adaadb011d95
Florik3ks <floritrs@gmail.com>
2021-03-26 09:50:49 +0100

    changed order of send_good_meme embed description

--- cogs/memes.py
+++ cogs/memes.py
@@ -305,13 +305,12 @@ class Memes(commands.Cog):
 
         channel = self.bot.get_channel(config.GOOD_MEMES_CHANNEL_ID)
         e = discord.Embed()
-        e.description = ""
+        e.description = f"[Link zur Nachricht]({msg.jump_url})\n"
         if msg.reference != None: 
             # TODO vereinfachbar in v1.7
             c = self.bot.get_channel(msg.reference.channel_id)
             m = await c.fetch_message(msg.reference.message_id)
-            e.description += f"[bezieht sich auf ...]({m.jump_url})\n"
-        e.description += f"[Link zur Nachricht]({msg.jump_url})"
+            e.description += f"[Bezieht sich auf ...]({m.jump_url})\n"
         e.set_author(name=msg.author,
                      icon_url=msg.author.avatar_url)
         e.color = msg.guild.get_member(
